3cfc60d625c5c712c406a1831b29a8c9702ba546
Steve Leigh <steve.leigh@readify.net>
2020-04-22 13:12:25 +1000

    Demo to supports exaggerated elevation

---
+++ SampleProject/Assets/Microsoft.Maps.Unity.Examples/ElevationExaggerationExample/Scripts/ElevationScaleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevationScaleController : MonoBehaviour
+{
+    [SerializeField][Range(0, 4)] float elevationScale = 1;
+
+    private int elevationScaleId;
+    
+    void Awake()
+    {
+        elevationScaleId = Shader.PropertyToID("_ElevationScale");
+    }
+
+    void Update()
+    {
+        Shader.SetGlobalFloat(elevationScaleId, elevationScale);
+    }
+}
